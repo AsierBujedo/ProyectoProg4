18b125855720e300296e6a23fe9c6b0953b4f6d1
Asier Bujedo Álvarez <asier.bujedo@opendeusto.es>
2022-03-19 19:28:54 +0100

    Arreglo sentencias SQL

--- main.c
+++ main.c
@@ -10,14 +10,6 @@
 #include "handler/DBH.h"
 
 int main() {
-	char *sql1 = "DROP TABLE IF EXISTS SUPERMERCADO;"
-					"CREATE TABLE SUPERMERCADO ("
-						"COD_S INT PRIMARY KEY NOT NULL,"
-						"NOM_S VARCHAR(25),"
-						"DIR_S VARCHAR(35),"
-						"TLF_S INT,"
-						"METROS_CUAD_S DOUBLE,"
-						"COD_CIU INT FOREIGN KEY REFERENCES CIUADAD (COD_CIU));";
 
 	char *sql2 = "DROP TABLE IF EXISTS PROVINCIA;"
 					 "CREATE TABLE PROVINCIA ("
@@ -28,7 +20,16 @@ int main() {
 				 "CREATE TABLE CIUDAD ("
 				 	 "COD_CIU INT PRIMARY KEY NOT NULL,"
 				 	 "NOM_CIU VARCHAR(25),"
-				 	 "COD_PROV INT FOREIGN KEY REFERENCES PROVINCIA (COD_PROV));";
+				 	 "COD_PROV INT, FOREIGN KEY (COD_PROV) REFERENCES PROVINCIA (COD_PROV));";
+
+	char *sql1 = "DROP TABLE IF EXISTS SUPERMERCADO;"
+					"CREATE TABLE SUPERMERCADO ("
+						"COD_S INT PRIMARY KEY NOT NULL,"
+						"NOM_S VARCHAR(25),"
+						"DIR_S VARCHAR(35),"
+						"TLF_S INT,"
+						"METROS_CUAD_S DOUBLE,"
+						"COD_CIU INT, FOREIGN KEY (COD_CIU) REFERENCES CIUADAD (COD_CIU));";
 
 	char *sql4 = "DROP TABLE IF EXISTS EMPLEADO;"
 				 "CREATE TABLE EMPLEADO ("
@@ -37,7 +38,7 @@ int main() {
 				 	 "SALARIO_EMP DOUBLE,"
 				 	 "FECHA_NCTO_EMP DATE,"
 				 	 "DIR_EMP VARCHAR(35),"
-				 	 "DNI_JEFE VARCHAR(9) FOREIGN KEY REFENRENCES EMPLEADO (DNI_EMP));";
+				 	 "DNI_JEFE VARCHAR(9), FOREIGN KEY (DNI_JEFE) REFERENCES EMPLEADO (DNI_EMP));";
 
 	char *sql5 = "DROP TABLE IF EXISTS PRODUCTO;"
 				 "CREATE TABLE PRODUCTO ("
@@ -51,27 +52,27 @@ int main() {
 				 	 "COD_S INT NOT NULL, "
 				 	 "ID_PROD INT NOT NULL, "
 				 	 "PRIMARY KEY(COD_S, ID_PROD),"
-				 	 "COD_S FOREIGN KEY REFERENCES SUPERMERCADO (COD_S),"
-				 	 "ID_PROD FOREIGN KEY REFERENCES PRODUCTO (ID_PROD));";
+				 	 "FOREIGN KEY (COD_S) REFERENCES SUPERMERCADO (COD_S),"
+				 	 "FOREIGN KEY (ID_PROD) REFERENCES PRODUCTO (ID_PROD));";
 
 	char *sql7 = "DROP TABLE IF EXISTS TRABAJA;"
 				 "CREATE TABLE TRABAJA("
-				 "COD_S INT NOT NULL,"
+				 "COD_S	INT NOT NULL,"
 				 "DNI_EMP VARCHAR(9) NOT NULL, "
-				 "PRIMARY KEY (COD_S, DNI_EMP),"
-				 "COD_S FOREIGN KEY REFERENCES SUPERMERCADO (COD_S),"
-				 "DNI_EMP FOREIGN KEY REFERENCES EMPLEADO (DNI_EMP));";
+				 "PRIMARY KEY (COD_S, DNI_EMP) "
+				 "FOREIGN KEY (COD_S) REFERENCES SUPERMERCADO (COD_S),"
+				 "FOREIGN KEY (DNI_EMP) REFERENCES EMPLEADO (DNI_EMP));";
 
 	sqlite3 *db;
 	db = initDB("DeustoMarket.db");
-	executeStatement(sql1, db);
 	executeStatement(sql2, db);
 	executeStatement(sql3, db);
+	executeStatement(sql1, db);
 	executeStatement(sql4, db);
 	executeStatement(sql5, db);
 	executeStatement(sql6, db);
 	executeStatement(sql7, db);
-
+	closeDB(db);
 
 
 	return 0;
