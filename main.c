d7249a3c4370cb810b3b4ef7b33849bbcb068d17
Asier Bujedo Álvarez <asier.bujedo@opendeusto.es>
2022-03-19 17:53:59 +0100

    handler implementation

--- main.c
+++ main.c
@@ -9,56 +9,27 @@
 #include <stdbool.h>
 #include "handler/DBH.h"
 
-void initBD(){
-
-	sqlite3 *db;
-	char *zErrMsg = 0;
-//	sqlite3_stmt *stmt;
-
-	int result = sqlite3_open("desutoMarketBD.sqlite", &db);
-	if (result != SQLITE_OK) {
-	printf("Error opening database\n");
-
-	// Tabla SUPERMERCADO
+int main() {
 	char *sql1 = "DROP TABLE IF EXISTS SUPERMERCADO;"
-				"CREATE TABLE SUPERMERCADO ("
-					"COD_S INT PRIMARY KEY NOT NULL,"
-					"NOM_S VARCHAR(25),"
-					"DIR_S VARCHAR(35),"
-					"TLF_S INT,"
-					"METROS_CUAD_S DOUBLE,"
-					"COD_CIU INT FOREIGN KEY REFERENCES CIUADAD (COD_CIU));";
+					"CREATE TABLE SUPERMERCADO ("
+						"COD_S INT PRIMARY KEY NOT NULL,"
+						"NOM_S VARCHAR(25),"
+						"DIR_S VARCHAR(35),"
+						"TLF_S INT,"
+						"METROS_CUAD_S DOUBLE,"
+						"COD_CIU INT FOREIGN KEY REFERENCES CIUADAD (COD_CIU));";
 
-	// Ejecutar la sentencia SQL
-	int rc = sqlite3_exec(db, sql1, callback, 0, &zErrMsg);
-
-	if( rc != SQLITE_OK ){
-	   fprintf(stderr, "SQL error: %s\n", zErrMsg);
-	   sqlite3_free(zErrMsg);
-	} else {
-	   fprintf(stdout, "La tabla se ha creado satisfactoriamente\n");
-	}
-
-	// Tabla PROVINCIA
 	char *sql2 = "DROP TABLE IF EXISTS PROVINCIA;"
-				 "CREATE TABLE PROVINCIA ("
-				 	 "COD_PROV INT PRIMARY KEY NOT NULL,"
-				 	 "NOM_PROV VARCHAR(25));";
+					 "CREATE TABLE PROVINCIA ("
+					 	 "COD_PROV INT PRIMARY KEY NOT NULL,"
+					 	 "NOM_PROV VARCHAR(25));";
 
-	// Ejecutar la sentencia SQL
-	// ...
-
-	// Tabla CIUDAD
 	char *sql3 = "DROP TABLE IF EXISTS CIUDAD;"
 				 "CREATE TABLE CIUDAD ("
 				 	 "COD_CIU INT PRIMARY KEY NOT NULL,"
 				 	 "NOM_CIU VARCHAR(25),"
 				 	 "COD_PROV INT FOREIGN KEY REFERENCES PROVINCIA (COD_PROV));";
 
-	// Ejecutar la sentencia SQL
-		// ...
-
-	// Tabla EMPLEADO
 	char *sql4 = "DROP TABLE IF EXISTS EMPLEADO;"
 				 "CREATE TABLE EMPLEADO ("
 				 	 "DNI_EMP VARCHAR(9) PRIMARY KEY NOT NULL,"
@@ -68,10 +39,6 @@ void initBD(){
 				 	 "DIR_EMP VARCHAR(35),"
 				 	 "DNI_JEFE VARCHAR(9) FOREIGN KEY REFENRENCES EMPLEADO (DNI_EMP));";
 
-	// Ejecutar la sentencia SQL
-		// ...
-
-	// Tabla PRODUCTO
 	char *sql5 = "DROP TABLE IF EXISTS PRODUCTO;"
 				 "CREATE TABLE PRODUCTO ("
 				 	 "ID_PROD INT PRIMARY KEY NOT NULL,"
@@ -79,10 +46,6 @@ void initBD(){
 				 	 "PRECIO_PROD DOUBLE,"
 				 	 "DESC_PROD VARVHAR(35));";
 
-	// Ejecutar la sentencia SQL
-		// ...
-
-	// Tabla VENDE
 	char *sql6 = "DROP TABLE IF EXISTS VENDE;"
 				 "CREATE TABLE VENDE ("
 				 	 "COD_S INT NOT NULL, "
@@ -91,10 +54,6 @@ void initBD(){
 				 	 "COD_S FOREIGN KEY REFERENCES SUPERMERCADO (COD_S),"
 				 	 "ID_PROD FOREIGN KEY REFERENCES PRODUCTO (ID_PROD));";
 
-	// Ejecutar la sentencia SQL
-		// ...
-
-	// Tabla TRABAJA
 	char *sql7 = "DROP TABLE IF EXISTS TRABAJA;"
 				 "CREATE TABLE TRABAJA("
 				 "COD_S INT NOT NULL,"
@@ -103,14 +62,18 @@ void initBD(){
 				 "COD_S FOREIGN KEY REFERENCES SUPERMERCADO (COD_S),"
 				 "DNI_EMP FOREIGN KEY REFERENCES EMPLEADO (DNI_EMP));";
 
-	// Ejecutar la sentencia SQL
-		// ...
+	sqlite3 *db;
+	db = initDB("DeustoMarket.db");
+	executeStatement(sql1, db);
+	executeStatement(sql2, db);
+	executeStatement(sql3, db);
+	executeStatement(sql4, db);
+	executeStatement(sql5, db);
+	executeStatement(sql6, db);
+	executeStatement(sql7, db);
 
-	return result;
-}
 
-int main() {
-	initBD();
+
 	return 0;
 }
 
